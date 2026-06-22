4a587bafbf0afef96dc5e16cdcd6498b5f5969d0
DGKL <2136315951@qq.com>
2026-06-22 16:24:28 +0800

    init smart law firm agent project

---
+++ src/main/java/com/example/dgkl/DgklApplication.java
@@ -0,0 +1,13 @@
+package com.example.dgkl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgklApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DgklApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dgkl/DgklApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dgkl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgklApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
